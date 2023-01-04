016dd3bbc450c2c88133e39ec4a85f65e3b47d44
이다원 <lyumilove9@gmail.com>
2023-01-04 10:43:47 +0900

    [Feat] 타임라인 라이브러리 설치 및 적용

--- src/features/shop/screens/ShopScreen.tsx
+++ src/features/shop/screens/ShopScreen.tsx
@@ -1,5 +1,82 @@
+import React from 'react';
+import Timeline from 'react-native-beautiful-timeline';
+import styled from 'styled-components/native';
+
+const data = [
+  {
+    date: 1574342522000,
+    data: [
+      {
+        title: '오모스 킥오프 미팅',
+        subtitle: '1시 30분',
+        date: 1574342522000,
+      },
+      {
+        title: 'javascript 스터디',
+        subtitle: '2시반',
+        date: 1574342501000,
+      },
+    ],
+  },
+  {
+    date: 1574248261000,
+    data: [
+      {
+        title: '클론코딩',
+        subtitle: '인스타 클론코딩',
+        date: 1574248261000,
+      },
+    ],
+  },
+  {
+    date: 1574125621000,
+    data: [
+      {
+        title: '타임라인',
+        subtitle: '적용',
+        date: 1574125621000,
+      },
+    ],
+  },
+  {
+    date: 1574125621000,
+    data: [
+      {
+        title: '타임라인',
+        subtitle: '적용',
+        date: 1574125621000,
+      },
+    ],
+  },
+  {
+    date: 1574125621000,
+    data: [
+      {
+        title: '타임라인',
+        subtitle: '적용',
+        date: 1574125621000,
+      },
+    ],
+  },
+];
+
 const ShopScreen = () => {
-  return <></>;
+  return (
+    <Container>
+      <Timeline
+        data={data}
+        style={{
+          backgroundColor: 'white',
+          paddingTop: 20,
+        }}
+        dashColor="black"
+      />
+    </Container>
+  );
 };
 
 export default ShopScreen;
+
+const Container = styled.View`
+  background-color: white;
+`;
